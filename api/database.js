646b4063cada5343ad5b6874d7b47b90a8e84bd1
bh4t1a <rishabh.suman.soft.game@gmail.com>
2022-07-28 16:44:50 +0530

    My commit 2

--- api/database.js
+++ api/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Ezio_Auditore47#',
+    password: '',
     database: 'sakila'
 });
 connection.connect(function(err) {
